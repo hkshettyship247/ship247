e93b1a276fa0c81df140b171b9f594dc6e417be7
hkshettyship247 <152865928+hkshettyship247@users.noreply.github.com>
2024-02-25 13:56:56 +0530

    pricingui + cma price list + eta auto - hemanth
    pricingui + cma price list + eta auto - admin, employees, suppliers

--- resources/views/admin/sea_schedules/create.blade.php
+++ resources/views/admin/sea_schedules/create.blade.php
@@ -166,7 +166,7 @@
 
                             <div class="form-field">
                                 <label for="eta" class="form-label">ETA</label>
-                                <input type="date" id="eta" name="eta" required
+                                <input type="date" id="eta" name="eta" required readonly
                                        class="form-input small-input mt-2 w-full block" value="{{ old('eta') }}">
                                 @error('eta')
                                 <span>{{ $message }}</span>
@@ -219,6 +219,10 @@
 @section('footer-scripts')
     <script>
         $(document).ready(function () {
+			
+			setETDMin();
+			setValidDateMin();
+			
             $('#origin_charges_included').change(function () {
                 toggleOriginCharges($(this).is(':checked'));
             })
@@ -242,6 +246,56 @@
                     $('#destination_charges').prop('readonly', false).prop('disabled', false);
                 }
             }
+			
+			$('#etd').change(function () {
+                setETA();
+            })
+			
+			$('#tt').change(function () {
+                setETA();
+            })
+			
+			function setETA(){
+				
+				etd=$('#etd').val();
+				tt=$('#tt').val();
+				if(etd != null && tt > 0){
+					const date = new Date(etd);
+					var newDate = new Date();
+					newDate.setTime(date.getTime() + (tt * 24 * 60 * 60 * 1000) );
+					dateday="0"+(newDate.getDate());
+					dateday=dateday.slice(-2);
+					datemonth="0"+(newDate.getMonth()+1);
+					datemonth=datemonth.slice(-2);
+					eta=newDate.getFullYear()+"-"+datemonth+"-"+dateday;
+					$('#eta').val(eta);
+				}
+				else{
+					$('#eta').val('');
+				}
+				
+			}
+			
+			function setETDMin(){
+				const date = new Date();
+				dateday="0"+(date.getDate());
+				dateday=dateday.slice(-2);
+				datemonth="0"+(date.getMonth()+1);
+				datemonth=datemonth.slice(-2);
+				etdmin=date.getFullYear()+"-"+datemonth+"-"+dateday;
+				 $('#etd').prop('min', etdmin);
+			}
+			
+			function setValidDateMin(){
+				const date = new Date();
+				dateday="0"+(date.getDate());
+				dateday=dateday.slice(-2);
+				datemonth="0"+(date.getMonth()+1);
+				datemonth=datemonth.slice(-2);
+				etdmin=date.getFullYear()+"-"+datemonth+"-"+dateday;
+				 $('#valid_till').prop('min', etdmin);
+			}
         });
+		
     </script>
 @endsection
